bf602c514adf90b33562f8b37d7f9e5455ff2825
LegionAnkit <ankitpandey1206@gmail.com>
2022-08-10 15:30:29 +0530

    Upadted executor java file

--- Demo/src/com/lti/inheritance/single/Executor.java
+++ Demo/src/com/lti/inheritance/single/Executor.java
@@ -8,8 +8,9 @@ public class Executor {
 		
 		Television t1 = new Television("Sony");
 		t1 = new Television("Samsung");
-		t2 = new Television("MI";
+		t2 = new Television("MI");
 		System.out.println(t1.getCompanyName());
+		System.out.println(t2.getCompanyName());
 
 	}
 
